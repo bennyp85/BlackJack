ff0344dde6d854bb2c5e564504b2693ad2b3e59d
Ben Pritchard <benjamintp.1985@gmail.com>
2022-05-11 08:38:08 +0000

    Cards and game play created with inputs

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 class Game:
-  pass
+  def __init__(self, playerScore=None, dealerScore=None):
+    self.playerScore = playerScore
+    self.dealerScore = dealerScore
+
+  def WhoWon(self):
+    if self.playerScore > self.dealerScore and self.playerScore <= 21:
+      return "Player Won"
+    else:
+      return "Dealer Won"
 
 class Player:
-  pass
+  def __init__(self, score=None):
+    self.score = score
 
+  def PlayerScore(self):
+    score = input("Player Card Count: ")
+    return int(score)
+    
 class Dealer:
-  pass
+  def __init__(self, score=None):
+    self.score = score
+
+  def DealerScore(self):
+    score = input("Dealer Card Count: ")
+    return int(score)
 
 class Deck:
   pass
+
+player = Player()
+pScore = player.PlayerScore()
+
+dealer = Dealer()
+dScore = dealer.DealerScore()
+
+game = Game(pScore, dScore)
+print(game.WhoWon())
